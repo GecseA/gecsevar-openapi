9f349da9fb9e8bd170cb9794172dde9b79c9e7e0
AstrA <gecsevar@gmail.com>
2025-04-10 20:34:37 +0200

    Create default sets

--- app/src/main/kotlin/hu/gecsevar/openapi/app/Application.kt
+++ app/src/main/kotlin/hu/gecsevar/openapi/app/Application.kt
@@ -1,26 +1,51 @@
 package hu.gecsevar.openapi.app
 
-import hu.gecsevar.database.view.ArticleModel
+import hu.gecsevar.database.view.TestDto1
 import kotlinx.datetime.Clock
+import kotlinx.datetime.LocalDate
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
 import kotlinx.serialization.json.Json
+import kotlin.uuid.ExperimentalUuidApi
+import kotlin.uuid.Uuid
 
+@OptIn(ExperimentalUuidApi::class)
 fun main(args: Array<String>) {
 
     println("Hello World!")
 
-    val article = ArticleModel(
-        id = 0,
-        published = false,
-        slug = "my-slug",
-        categories = listOf("article"),
-        creator = "AstrA",
-        createTime = Clock.System.now(),
-        title = "My Title",
-        cardImageUrl = "",
-        bannerImageUrl = "",
-        description = "Good description",
-        content = "Fun content"
+    val dto1 = TestDto1(
+        id = 111,
+        myBool = true,
+        myString = "Hello World!",
+        myStringId = Uuid.random().toString(),
+        myNumber = 234234,
+        myNumber2 = 234235544,
+        aDateTime = Clock.System.now(),
+        aDate = Clock.System.now().toLocalDateTime(TimeZone.UTC).date,
+        aTime = "wrong",
+        myEnum = TestDto1.MyEnum.MY_ENUM_1,
+        myArray = listOf(
+            "string 1",
+            "string 2",
+            "string 3",
+        )
     )
 
-    println(Json.encodeToString(article))
+    println(Json.encodeToString(dto1))
+//    val article = ArticleModel(
+//        id = 0,
+//        published = false,
+//        slug = "my-slug",
+//        categories = listOf("article"),
+//        creator = "AstrA",
+//        createTime = Clock.System.now(),
+//        title = "My Title",
+//        cardImageUrl = "",
+//        bannerImageUrl = "",
+//        description = "Good description",
+//        content = "Fun content"
+//    )
+
+//    println(Json.encodeToString(article))
 }
